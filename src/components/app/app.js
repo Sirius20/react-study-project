9527bd87b5da88523c4755efa38b7d534c044110
Natalya <nataldenal89@gmail.com>
2021-02-16 17:30:57 +0500

    Стартовое состояние проекта. Модули и стили

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import AppHeader from '../app-header/app-header';
+import SearchPanel from '../search-panel/search-panel';
+import PostStatusFilter from '../post-status-filter/post-status-filter';
+import PosList from '../post-list/post-list';
+import PostAddForm from "../post-add-form/post-add-form";
+
+import './app.css';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <div>
+                <PosList/>
+            </div>
+            <div>
+                <PostAddForm/>
+            </div>
+        </div>
+    )
+}
+
+export default App;
